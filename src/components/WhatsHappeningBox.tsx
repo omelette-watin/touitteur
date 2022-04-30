4e99a8bcf11a1fe22296c9da7821d6dcd0903000
Nicolas <pnwa@icloud.com>
2022-04-30 12:42:56 +0200

    add a link in what's happening image

--- src/components/WhatsHappeningBox.tsx
+++ src/components/WhatsHappeningBox.tsx
@@ -1,6 +1,7 @@
 import useAuth from "@/hooks/useAuth"
 import TweetForm from "./TweetForm"
 import Image from "next/image"
+import Link from "next/link"
 
 const WhatsHappeningBox = () => {
   const { user } = useAuth()
@@ -8,13 +9,17 @@ const WhatsHappeningBox = () => {
   return (
     <div className="flex w-full items-start border-b border-gray-700 p-3 scrollbar-hide">
       <div className="flex-shrink-0">
-        <Image
-          src={user?.urlAvatar || "/avatars/default.svg"}
-          alt="Your avatar"
-          className="rounded-full"
-          width={48}
-          height={48}
-        />
+        <Link href={`/${user?.username}`}>
+          <a>
+            <Image
+              src={user?.urlAvatar || "/avatars/default.svg"}
+              alt="Your avatar"
+              className="rounded-full"
+              width={48}
+              height={48}
+            />
+          </a>
+        </Link>
       </div>
       <TweetForm replying={null} />
     </div>
