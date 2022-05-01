7d12d815304bedbe4df175779a601258c2200908
Nicolas <pnwa@icloud.com>
2022-05-01 20:02:35 +0200

    adapt to posted tweet type

--- src/types/tweet.ts
+++ src/types/tweet.ts
@@ -6,17 +6,20 @@ export interface TweetType {
   authorId?: string
   author: UserType
   type?: string
-  createdAt: Date
+  createdAt: Date | string
   _count: { likes: number; retweets: number; replies: number }
   originalTweetId?: string | null
-  originalTweet?: TweetType | null
+  originalTweet?:
+    | TweetType
+    | null
+    | { id: string; author: { username: string } }
 }
 
 export interface TweetEventType {
   id: string
   authorId: string
   author: UserType
-  createdAt: Date
+  createdAt: Date | string
   targetTweetId: string
   targetTweet: TweetType
   type: "tweet" | "reply" | "like" | "retweet"
