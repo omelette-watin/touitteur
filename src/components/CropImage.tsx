8038f0311498ee7d8028629cfa17142640fc5571
Nicolas <pnwa@icloud.com>
2022-05-02 21:46:31 +0200

    remove imported image on cancel crop

--- src/components/CropImage.tsx
+++ src/components/CropImage.tsx
@@ -1,3 +1,4 @@
+import useAuth from "@/hooks/useAuth"
 import getCroppedImg from "@/utils/getCroppedImage"
 import { useCallback, useState } from "react"
 import Cropper from "react-easy-crop"
@@ -16,10 +17,16 @@ const CropImage = ({
   setCroppedImage,
   setCroppedUrl,
 }: CropImageProps) => {
+  const { user } = useAuth()
   const [crop, setCrop] = useState<Point>({ x: 0, y: 0 })
   const [zoom, setZoom] = useState(1)
   const [croppedAreaPixels, setCroppedAreaPixels] = useState(null)
   const [rotation, setRotation] = useState(0)
+  const handleCancel = () => {
+    setCroppedUrl(user?.urlAvatar)
+    setCroppedImage(null)
+    closeCrop()
+  }
   const onCropComplete = useCallback(
     (_croppedArea: any, croppedAreaPixels: any) => {
       setCroppedAreaPixels(croppedAreaPixels)
@@ -99,7 +106,7 @@ const CropImage = ({
       <div className="flex items-center justify-center space-x-4 pt-8 text-lg">
         <button
           type="button"
-          onClick={closeCrop}
+          onClick={handleCancel}
           className="w-fit self-center rounded-full border px-6 py-1 hover:border-red-600 hover:bg-red-600/10 hover:text-red-600"
         >
           Cancel
