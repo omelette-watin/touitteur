6ba88ce248cce8144b323e5f1d6c1e561537d9ef
Nicolas <pnwa@icloud.com>
2022-05-11 22:12:16 +0200

    change spinner to bird

--- src/components/LoadingPage.tsx
+++ src/components/LoadingPage.tsx
@@ -1,9 +1,14 @@
-import Loading from "./ui/Loading"
+import Image from "next/image"
 
 const LoadingPage = (): JSX.Element => {
   return (
     <div className="flex min-h-screen items-center justify-center">
-      <Loading color="#00AAEC" />
+      <Image
+        src="/twouitteur.svg"
+        width={80}
+        height={80}
+        alt="Touitteur's log"
+      />
     </div>
   )
 }
