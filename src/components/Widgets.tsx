4495494c3730d08e8623b5b30448ed4d017d7eac
Nicolas <pnwa@icloud.com>
2022-05-18 04:44:13 +0200

    stycky searchbar to the right

--- src/components/Widgets.tsx
+++ src/components/Widgets.tsx
@@ -6,7 +6,9 @@ import Widget from "./Widget"
 const Widgets = () => {
   return (
     <div className="my-3 ml-4 hidden w-[350px] space-y-4 lg:block">
-      <SearchBar />
+      <div className="sticky top-0 z-10 w-full bg-black py-2">
+        <SearchBar />
+      </div>
       <Widget>
         <TrendingHashtags />
       </Widget>
