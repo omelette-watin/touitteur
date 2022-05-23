3746a66fcb08dddca4d865abcc4b228cd721a3c6
Nicolas <pnwa@icloud.com>
2022-05-23 18:12:46 +0200

    width fit content

--- src/components/ui/Tooltip.tsx
+++ src/components/ui/Tooltip.tsx
@@ -2,7 +2,7 @@ import { ReactChild } from "react"
 
 const Tooltip = ({ children }: { children: ReactChild }) => {
   return (
-    <div className="absolute -bottom-7 -right-7 hidden w-max rounded bg-gray-700/80 py-1 px-2 text-xs text-white shadow-sm group-hover:block">
+    <div className="absolute -bottom-7 -right-7 hidden w-fit rounded bg-gray-700/80 py-1 px-2 text-xs text-white shadow-sm group-hover:block">
       {children}
     </div>
   )
